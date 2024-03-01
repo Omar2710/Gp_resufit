61c569dfe7fa25a976e5c4c87532b33b9ca5c8c4
doniaSameh <97558647+doniaSameh@users.noreply.github.com>
2024-03-02 01:26:52 +0200

    Update applicant.module.ts

--- ATS-GradProject/backend/src/applicant/applicant.module.ts
+++ ATS-GradProject/backend/src/applicant/applicant.module.ts
@@ -5,10 +5,37 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Applicant } from './entities/applicant.entity';
 import { Resume } from './entities/resume.entity';
 import { ResumeController } from './resume.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { HttpModule } from '@nestjs/axios';
+import { OdooModule } from 'src/odoo/odoo.module';
+import { JobVacanyModule } from 'src/job-vacany/job-vacany.module';
+import { ResumeScreeningQuestionsAnswers } from './entities/resume-screening-questions-answers.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Applicant, Resume])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      Applicant,
+      Resume,
+      ResumeScreeningQuestionsAnswers,
+    ]),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads', // Specify destination folder
+        filename: (req, file, callback) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          callback(null, `${randomName}${file.originalname}`); // Generate unique filename
+        },
+      }),
+    }),
+    HttpModule,
+    OdooModule,
+    JobVacanyModule,
+  ],
+  exports: [TypeOrmModule, ApplicantService],
   controllers: [ApplicantController, ResumeController],
   providers: [ApplicantService],
 })
