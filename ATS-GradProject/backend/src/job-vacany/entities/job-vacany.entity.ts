505c385034e765b8b775cad16a2002be4ab9e97a
HabebaMohamedd12 <97558348+HabebaMohamedd12@users.noreply.github.com>
2024-03-02 01:32:56 +0200

    Update job-vacany.entity.ts

--- ATS-GradProject/backend/src/job-vacany/entities/job-vacany.entity.ts
+++ ATS-GradProject/backend/src/job-vacany/entities/job-vacany.entity.ts
@@ -1,13 +1,7 @@
-import {
-  Column,
-  Entity,
-  JoinTable,
-  ManyToMany,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { JobVacanyTechSkill } from './job-vacany-tech-skill.entity';
 import { Resume } from 'src/applicant/entities/resume.entity';
+import { JobVacanyScreeningQuestions } from './job-vacancy-screening-question.entity';
 
 @Entity()
 export class JobVacany {
@@ -17,22 +11,33 @@ export class JobVacany {
   @Column()
   title: string;
 
-  @Column()
+  @Column({ default: null })
   education: string;
-
-  @Column()
-  language: string;
 
   @Column()
   yearsOfExperience: string;
 
-  @Column()
+  @Column({ type: 'text', default: null })
   jobDescription: string;
 
-  @OneToMany(() => JobVacanyTechSkill, (d) => d.job_vacancy)
+  @Column()
+  odooJobId: number;
+
+  @OneToMany(() => JobVacanyTechSkill, (d) => d.job_vacancy, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
   job_vacancy_tech_skill: JobVacanyTechSkill[];
 
-  @ManyToMany(() => Resume)
-  @JoinTable()
+  @OneToMany(() => JobVacanyScreeningQuestions, (d) => d.job_vacancy, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  job_vacancy_screening_questions: JobVacanyScreeningQuestions[];
+
+  @OneToMany(() => Resume, (resume) => resume.jobVacancy, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
   resumes: Resume[];
 }
