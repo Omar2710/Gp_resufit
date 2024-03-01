650bcc5e512c39824e900ccc7ef244dbd2174832
doniaSameh <97558647+doniaSameh@users.noreply.github.com>
2024-03-02 01:24:03 +0200

    Update applicant.controller.ts

--- ATS-GradProject/backend/src/applicant/applicant.controller.ts
+++ ATS-GradProject/backend/src/applicant/applicant.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { ApplicantService } from './applicant.service';
 import { CreateApplicantDto } from './dto/create-applicant.dto';
 import { UpdateApplicantDto } from './dto/update-applicant.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthApplicantGuard } from 'src/auth/auth guard/auth-applicant.guard';
 
+@UseGuards(AuthApplicantGuard)
 @Controller('applicant')
 export class ApplicantController {
   constructor(private readonly applicantService: ApplicantService) {}
 
   @Post()
-  create(@Body() createApplicantDto: CreateApplicantDto) {
-    return this.applicantService.create(createApplicantDto);
+  @UseInterceptors(FileInterceptor('resumeFile'))
+  create(
+    @Body() createApplicantDto: CreateApplicantDto,
+    @UploadedFile() resumeFile: Express.Multer.File,
+    @Request() req,
+  ) {
+    if (req.user) {
+      createApplicantDto.userName = req.user.userName;
+    }
+    return this.applicantService.create(createApplicantDto, resumeFile);
   }
 
   @Get()
@@ -23,12 +46,26 @@ export class ApplicantController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateApplicantDto: UpdateApplicantDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateApplicantDto: UpdateApplicantDto,
+  ) {
     return this.applicantService.update(+id, updateApplicantDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.applicantService.remove(+id);
+  }
+
+  @Get('/resume/:jobVacancyId')
+  getResumeByJobVacancy(
+    @Param('jobVacancyId') jobVacancyId: number,
+    @Request() req,
+  ) {
+    return this.applicantService.getResumeForJobVacancy({
+      jobVacancyId,
+      userName: req.user?.userName,
+    });
   }
 }
