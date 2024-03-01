5e34a16157f578a30e1a3c3b1f3841d4724400d5
MohamedAshraf <97558470+MohamedAshraf630@users.noreply.github.com>
2024-03-02 01:43:36 +0200

    Update admin.service.ts

--- ATS-GradProject/backend/src/admin/admin.service.ts
+++ ATS-GradProject/backend/src/admin/admin.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from './entities/admin.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AdminService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+export class AdminService implements OnModuleInit {
+  constructor(
+    @InjectRepository(Admin)
+    private adminRepository: Repository<Admin>,
+  ) {}
+  //runs with startup of the application and creates admin details in database (seeding database)
+  async onModuleInit() {
+    await this.create({
+      firstName: 'root',
+      lastName: 'root',
+      username: 'root',
+      password: 'root',
+    });
   }
 
-  findAll() {
-    return `This action returns all admin`;
+  async create(createAdminDto: CreateAdminDto) {
+    const userExists = await this.findOneByUsername(createAdminDto.username);
+    if (userExists) {
+      return;
+    }
+    const saltRounds = 10;
+    const password = await bcrypt.hash(createAdminDto.password, saltRounds);
+    const admin = this.adminRepository.create({ ...createAdminDto, password });
+    return await this.adminRepository.save(admin);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} admin`;
+  async findAll() {
+    return await this.adminRepository.find();
   }
 
-  update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
+  async findOneByUsername(username: string) {
+    return await this.adminRepository.findOne({ where: { username } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} admin`;
+  async findOne(id: number) {
+    return await this.adminRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, updateAdminDto: UpdateAdminDto) {
+    const admin = await this.findOne(id);
+
+    if (!admin) {
+      throw new NotFoundException(`Admin with ID ${id} not found`);
+    }
+
+    return this.adminRepository.save({ id, ...updateAdminDto });
+  }
+
+  async remove(id: number) {
+    return await this.adminRepository.delete({ id });
   }
 }
